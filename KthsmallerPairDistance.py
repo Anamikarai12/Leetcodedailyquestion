f426f473edab247ccb4bca64cf5e6d7a72d57ad3
Anamikarai12 <162546819+Anamikarai12@users.noreply.github.com>
2024-08-15 10:21:07 +0530

    Create KthsmallerPairDistance.py

---
+++ KthsmallerPairDistance.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def smallestDistancePair(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort()
+        
+        def count_pairs(mid):
+            count = 0
+            left = 0
+            for right in range(len(nums)):
+                while nums[right] - nums[left] > mid:
+                    left += 1
+                count += right - left
+            return count
+        
+        low, high = 0, nums[-1] - nums[0]
+        while low < high:
+            mid = (low + high) // 2
+            if count_pairs(mid) < k:
+                low = mid + 1
+            else:
+                high = mid
+        
+        return low
+
+a=[1,3,1]
+k=1
+sol=Solution()
+b=sol.smallestDistancePair(a,k)
+print(b)
